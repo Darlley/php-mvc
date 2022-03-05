941b14cb17ab8b19a7bc85e35c079b1c9e2a2225
Darlley <systembrtt@gmail.com>
2022-03-05 14:01:14 -0400

    require_once de uma classe que mostra a requisição GET

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php 
 
+  require_once('app/core/index.php');
+
   $indexTemplate = file_get_contents('app/index.html');
+
+  $core = new Core;
+  $core->start($_GET); // query strings x url amigáveis)
+
   echo $indexTemplate;
